013480c441838942e86ab5b4e523aded1e7db072
Andymanvitale <42122885+Andymanvitale@users.noreply.github.com>
2022-09-13 11:03:54 -0400

    created db.sql

---
+++ db/db.sql
@@ -0,0 +1,28 @@
+CERATE DATABASE airports;
+use allAirports
+
+CREATE TABLE IF NOT EXISTS airports (
+    'Facility Type' STR,
+    'ID' str,
+    'State' str,
+    'County' str,	
+    'City' str,
+    'Name' str,
+    'Ownership' str,	
+    'Use' str,	
+    'ARP Latitude' float,	
+    'ARP Latitude Sec' float,	
+    'ARP Longitude' float,	
+    'ARP Longitude Sec' float,	
+    'Elevation' str,	
+    'Magnetic Variation' str,	
+    'Sectional' str,
+    'Tie In FSS Id' str,
+    'Tie In FSS Name' str,
+    'FSS Toll Free Number' str,
+    'Fuel Types' str,	
+    'ATCT' str,	
+    'UNICOM' str,	
+    'CTAF' str,	
+    'ICAO Id' str, 
+)
