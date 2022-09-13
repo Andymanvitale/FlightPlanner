54acd386811bf31e2bd1054b1407f9775762f650
Andymanvitale <42122885+Andymanvitale@users.noreply.github.com>
2022-09-12 21:00:43 -0400

    Added CSV file

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
-import csv
+import math
 
-startLoc = input("Where are you taking off from? ")
+from geopy import distance
 
-print(f"{startLoc}")
+p1 = (43.668613, 40.258916, 0.976)
+
+p2 = (43.658852, 40.250839, 1.475)
+
+flat_distance = distance.distance(p1[:2], p2[:2]).km
+
+print(flat_distance)
