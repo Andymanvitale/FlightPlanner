9b03457af6008e5a07a4f31fb2f6f4a187054f7b
andymanvitale <andrewvitale98@gmail.com>
2022-09-14 21:16:52 -0500

    Changed csv to pandas and started to work on getting airport code coordinates

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
-import csv
+import pandas as pd
 # Import a CSV file to read the names of the airports
-with open('../all-airport-data.csv', newline =' ') as airportcsv:
-    airportread = csv.reader(airportcsv, delimiter=' ')
-    for row in airportread:
-        print(' '.join(row))
+df = pd.read_csv('../all-airport-data.csv')
         
-
+userInput1 = input("Departure airport Code: ")
+userInput2 = input("Arrival airport code: ")
 # Create a variable for lat/lon for airport 1 and 2
-
+airport1 = df.iloc[userInput1]
 # Read the CSV file to get store the lat/lon 
 
 #Use GeoPy to calculate distance between the two airports
