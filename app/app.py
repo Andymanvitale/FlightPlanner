37794c7ef5efef18747a058fa25296925605592e
andymanvitale <andrewvitale98@gmail.com>
2022-09-19 20:06:41 -0500

    Fixed errors and have data being stored in variables

--- app/app.py
+++ app/app.py
@@ -6,8 +6,10 @@ userInput1 = input("Departure airport Code: ")
 userInput1.upper()
 userInput2 = input("Arrival airport code: ")
 # Create a variable for lat/lon for airport 1 and 2
-latInput1 = df[df["LocId"] == userInput1]
+latInput1 = df.ARPLatitude[df["LocId"] == userInput1]
+longInput1 = df.ARPLongitude[df["LocId"] == userInput1]
 print(latInput1)
+print(longInput1)
 # Read the CSV file to get store the lat/lon 
 # print(df.ARPLatitude + ' ' + df.ARPLongitude)
 #Use GeoPy to calculate distance between the two airports
