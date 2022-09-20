eb54a0aca906e2194e4acf45fa7c32b8409c24bd
andymanvitale <andrewvitale98@gmail.com>
2022-09-19 20:24:14 -0500

    latValue gets only the value not all the other table info

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import geopy as gp
 # Import a CSV file to read the names of the airports
 df = pd.read_csv('../all-airport-data.csv')
         
@@ -7,9 +8,11 @@ userInput1.upper()
 userInput2 = input("Arrival airport code: ")
 # Create a variable for lat/lon for airport 1 and 2
 latInput1 = df.ARPLatitude[df["LocId"] == userInput1]
-longInput1 = df.ARPLongitude[df["LocId"] == userInput1]
 print(latInput1)
-print(longInput1)
-# Read the CSV file to get store the lat/lon 
-# print(df.ARPLatitude + ' ' + df.ARPLongitude)
+
+longInput1 = df.ARPLongitude[df["LocId"] == userInput1]
 #Use GeoPy to calculate distance between the two airports
+foo = str(latInput1).split()
+latValue = foo[1]
+
+print(latValue)
