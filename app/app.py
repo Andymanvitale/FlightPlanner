105dbfac9c3c1d52ba9763272068f68d88615b72
andymanvitale <andrewvitale98@gmail.com>
2022-09-19 20:03:35 -0500

    Created latInput1 which selects the row of data for the airport identifer thats entered '

--- app/app.py
+++ app/app.py
@@ -3,9 +3,11 @@ import pandas as pd
 df = pd.read_csv('../all-airport-data.csv')
         
 userInput1 = input("Departure airport Code: ")
+userInput1.upper()
 userInput2 = input("Arrival airport code: ")
 # Create a variable for lat/lon for airport 1 and 2
-airport1 = df.iloc[userInput1]
+latInput1 = df[df["LocId"] == userInput1]
+print(latInput1)
 # Read the CSV file to get store the lat/lon 
-
+# print(df.ARPLatitude + ' ' + df.ARPLongitude)
 #Use GeoPy to calculate distance between the two airports
