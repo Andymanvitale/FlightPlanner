fda158864895c713c728f731b6675ee2fe0c0728
Andymanvitale <42122885+Andymanvitale@users.noreply.github.com>
2022-11-06 20:28:09 -0500

    Submit button kinda works... doesnt show any value but it submits something

--- env/FlightPlanner/FlightPlanner/forms.py
+++ env/FlightPlanner/FlightPlanner/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class airports(forms.Form):
-    startApt = forms.CharField(label = 'startapt', max_length = 3)
-    endApt = forms.CharField(label = 'endapt', max_length = 3)
+    startApt = forms.CharField(label = 'Starting Airport', max_length = 3)
+    endApt = forms.CharField(label = 'Destination', max_length = 3)
 
 class airportRoute(forms.ModelForm):
     class Meta:
