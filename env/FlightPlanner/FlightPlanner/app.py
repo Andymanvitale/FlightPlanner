0f7ca4a1b999ad70c0abf8e01ee97a1b870ef6de
Andymanvitale <42122885+Andymanvitale@users.noreply.github.com>
2022-12-08 16:40:33 -0500

    Working on metar

--- env/FlightPlanner/FlightPlanner/app.py
+++ env/FlightPlanner/FlightPlanner/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from django.shortcuts import render
 from forms import airports
 from getDistance import findDistance
+import requests
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -22,3 +24,15 @@ def startAirport():
 def getDis():
     calc = findDistance()
     return render_template('index.html', result = calc)
+
+req = requests.get("https://api.checkwx.com/metar/KJFK?x-api-key=5df00ce5a84046e4ad91f3cca5")
+
+@app.route('/')
+def metar():
+    try:
+        req.raise_for_status()
+        resp = json.loads(req.text)
+        return render_template('index.html', result = resp)
+
+    except requests.exceptions.HTTPError as e:
+        print(e)
